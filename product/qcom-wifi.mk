229cf797181a68712fc009eeebdc9f714b8aaff3
desalesouche <desalesouche@gmail.com>
2017-10-13 23:11:10 +0200

    oreo update
    Change-Id: Ie8e983c7ca1f4c46e4a8521b7b9db7d55d1f271b

--- product/qcom-wifi.mk
+++ product/qcom-wifi.mk
@@ -16,15 +16,14 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libwcnss_qmi \
     wcnss_service \
-		libwpa_client
+    libwpa_client
 
 PRODUCT_PACKAGES += \
     wificond \
-    wifilogd \
     wpa_supplicant \
     wpa_supplicant.conf
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
-    $(LOCAL_PATH)/configs/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini
+    $(LOCAL_PATH)/configs/WCNSS_qcom_cfg.ini:system/vendor/etc/wifi/WCNSS_qcom_cfg.ini
